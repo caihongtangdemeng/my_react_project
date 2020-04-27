375a096208b8046183b3bbe2be13ca9fff37eab0
zhuyan <1414412625@qq.com>
2020-04-27 20:43:21 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React,{Component} from 'react'
+import {Button} from 'antd'
 
 export default class App extends Component{
   render(){
     return (
       <div>
-        App
-        
+        <h2 style={{color:'pink'}}>App</h2>
+        <Button type="primary">Button</Button>
       </div>
     )
   }
