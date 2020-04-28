21925d202d9c17a9ae01d6aed6c63a5505d26f7a
zhuyan <1414412625@qq.com>
2020-04-28 21:54:30 +0800

    a

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -14,7 +14,19 @@ axios.interceptors.request.use((config)=>{
 })
 
 axios.interceptors.response.use(
-  
+  response=>{
+    return response.data
+  },
+  err=>{
+    let errMsg='未知错误 请联系管理员'
+    const {message}=err
+    if(message.indexOf('401')!==-1) errMsg='未登录或登录过期，请重新登录'
+    else if(message.indexOf('Network Error')!==-1) errMsg='网络出错，请检查网络'
+    else if(message.indexOf('timeout')!==-1) errMsg='网络不稳定，连接超时'
+    //延时1s
+    msg.error(errMsg, 1)
+    return new Promise(()=>{})
+  }
 )
 
 
