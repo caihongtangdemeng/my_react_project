55489e2cc46e115dac32c31abd8bfe1947a3ff3a
zhuyan <1414412625@qq.com>
2020-04-28 09:21:33 +0800

    a

---
+++ day01_src antd/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import {Button } from 'antd'
+
+export default class App extends Component{
+  render(){
+    return (
+      <div>
+        <h2 style={{color:'pink'}}>App</h2>
+        <Button type="primary">Button</Button>
+      </div>
+    )
+  }
+}
