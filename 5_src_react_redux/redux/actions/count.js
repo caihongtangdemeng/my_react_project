4a6d38598b55d3c95aab52f5e53e9b1f8f917f39
zhuyan <1414412625@qq.com>
2020-04-30 08:22:27 +0800

    a

---
+++ 5_src_react_redux/redux/actions/count.js
@@ -0,0 +1,5 @@
+//该文件是创建和count相关的action对象{type:'',data:value}
+
+import {INCREMENT,DECREMENT}from '../action_types'
+ export const increment =value=>({type:INCREMENT,data:value})
+ export const decrement =value=>({type:DECREMENT,data:value})
---
+++ 5_src_react_redux/redux/store.js
@@ -0,0 +1,3 @@
+import {createStore} from 'redux'
+import countReducer from './reducers/count'
+export default createStore(countReducer)
