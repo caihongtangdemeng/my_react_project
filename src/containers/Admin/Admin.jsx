4d0c7a2dda32b1e6f792d3c1b7253ac86cec4153
zhuyan <1414412625@qq.com>
2020-05-05 00:46:24 +0800

    a

--- src/containers/Admin/Admin.jsx
+++ src/containers/Admin/Admin.jsx
@@ -1,15 +1,24 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
+import { Layout } from 'antd';
 import Header from './Header/Header'
+import './css/admin.less'
+const { Footer, Sider, Content } = Layout;
 
  class Admin extends Component{
   render(){
     if(!this.props.isLogin) return <Redirect to="/login"/>
     return (
-      <div>
-        <Header/>
-      </div>
+        <Layout className="admin-container">
+          <Sider>Sider</Sider>
+          <Layout>
+            <Header />
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+          </Layout>
+    </Layout>
+     
     )
   }
 }
--- src/containers/Admin/Header/Header.jsx
+++ src/containers/Admin/Header/Header.jsx
@@ -1,9 +1,68 @@
 import React,{Component} from 'react'
+import { Button ,Modal} from 'antd';
+import {	
+  FullscreenOutlined,
+	FullscreenExitOutlined,
+  ExclamationCircleOutlined 
+ } from '@ant-design/icons';
+import screenfull from 'screenfull'
+import './css/header.less'
+import demo from './demo.jpg'
+import { connect } from 'react-redux';
+import {deleteUserInfo} from '@/redux/actions/login'
 
-export default class Header extends Component{
+const { confirm } = Modal;
+ class Header extends Component{
+  state={
+    isFull:false
+  }
+  //退出登录
+  logout=()=>{
+    confirm({
+      title: '确定退出登录吗？',
+      icon: <ExclamationCircleOutlined />,
+      content: '退出后需重新登陆',
+      onOk:()=> {
+        this.props.deleteUserInfo();
+      },
+      
+    });
+  }
+  fullScreen=()=>{
+    screenfull.toggle() //切换
+  }
+  componentDidMount(){
+    screenfull.onchange(()=>{
+      const {isFull}=this.state
+      this.setState({isFull:!isFull})
+    })
+  }
   render(){
     return (
-      <div></div>
+      <div className="header">
+        <div className="header-top">
+          <Button size="small" onClick={this.fullScreen}>
+         { this.state.isFull ? <FullscreenExitOutlined /> : <FullscreenOutlined />}
+          </Button>
+          <span className="username">欢迎，xxx</span>
+          <Button type="link" size="small" onClick={this.logout}>退出登录</Button>
+        </div>
+        <div className="header-bottom">
+          <div className="bottom-left">
+            <span>首页</span>
+          </div>
+          <div className="bottom-right">
+            <span>2020年5月4日 00：00：00</span>
+            <img src={demo} alt=""/>
+            <span>多云转晴</span>
+            <span>温度：0~20℃</span>
+          </div>
+        </div>
+      </div>
     )
   }
 }
+export default connect(
+  state=>({}),
+  {deleteUserInfo}
+)(Header)
